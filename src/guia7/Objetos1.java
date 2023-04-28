75ccb13f135ce89ae37e885cbe8d2479a0e7bc8f
GamboaNicolasMuni <gamboa.nicolas2002@gmail.com>
2023-04-28 09:26:16 -0300

    primer Commit en la muni (tercero)

---
+++ src/guia7/Objetos1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7;
+/**
+/**
+ *
+ * @author adisant2
+ */
+public class Objetos1 {
+    
+    public static void main(String[] args){
+        
+        Persona pers1 = new Persona();
+        pers1.edad = 20;
+        pers1.nombre = "Nicolas";
+        pers1.pesoKG = 56;
+        
+         
+    }
+    
+}
---
+++ src/guia7/Persona.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guia7;
+
+/**
+ *
+ * @author adisant2
+ */
+class Persona {
+    public String nombre;
+    public int edad;
+    public int pesoKG;
+
+    public Persona(String nombre, int edad, int pesoKG) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.pesoKG = pesoKG;
+    }
+    
+    
+    public Persona(){
+         
+    }
+    
+   
+    
+}
